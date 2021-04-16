2facbfbc3cc0ac917777cb97afc4712dca450cbf
BizarreMaster16 <ruilucasporting@gmail.com>
2021-04-16 20:23:24 +0100

    Erro corrigido resultado aterado

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ function Calcular() {
     var numero2 = document.getElementById("valor2").value;
     console.log(document.getElementById("Operacao").value);
     console.log(numero1 + " + " + numero2 + " = " + (parseInt(numero1) + parseInt(numero2)));
-    document.getElementById("resultado").innerHtml = numero2;
+    document.getElementById("resultado").innerHTML = numero2;
 }
